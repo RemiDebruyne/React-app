be0f916fb32a662abca0fe1be8f2d978e2db1438
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-31 22:28:43 +0100

    Exercice : 3 button on click => increase, decrease, set to 0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let [count, setCount] = useState(0);
+
   return (
-    <div>
-      <ListGroup />
+    <div className="App">
+      {count}
+      <button onClick={() => setCount((count += 1))}>Increase</button>
+      <button onClick={() => setCount((count -= 1))}>Decrease</button>
+      <button onClick={() => setCount(0)}>Set to zero</button>
     </div>
   );
 }
